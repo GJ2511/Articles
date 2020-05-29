74540dada6ee9b3a344861c0c2de01c861a0f9e6
Gaurav Joshi <gaurav.joshi@onsolve.com>
2020-05-29 13:18:10 +0530

    added sign in and sign up template

--- src/component/SignIn.js
+++ src/component/SignIn.js
@@ -2,8 +2,37 @@ import React from 'react';
 
 function SignIn() {
     return (
-        <div>
-            <h2>SignIn</h2>
+        <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div className="card card-signin my-5 bg-light ">
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Sign In</h5>
+                        <form>
+                            <div className="form-group">
+                                <input
+                                    type="email"
+                                    className="form-control rounded-pill"
+                                    id="email"
+                                    aria-describedby="emailHelp"
+                                    placeholder="Email address"
+                                    autoFocus
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="password"
+                                    className="form-control rounded-pill"
+                                    id="password"
+                                    placeholder="Password "
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-primary btn-lg btn-block rounded-pill">
+                                Login
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/component/SignUp.js
+++ src/component/SignUp.js
@@ -2,8 +2,46 @@ import React from 'react';
 
 function SignUp() {
     return (
-        <div>
-            <h2>SignUp</h2>
+        <div className="row">
+            <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+                <div className="card card-signUp my-5 bg-light ">
+                    <div className="card-body">
+                        <h5 className="card-title text-center">Sign Up</h5>
+                        <form>
+                            <div className="form-group">
+                                <input
+                                    type="text"
+                                    className="form-control rounded-pill"
+                                    id="username"
+                                    aria-describedby="usernameHelp"
+                                    placeholder="User Name"
+                                    autoFocus
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="email"
+                                    className="form-control rounded-pill"
+                                    id="email"
+                                    aria-describedby="emailHelp"
+                                    placeholder="Email address"
+                                />
+                            </div>
+                            <div className="form-group">
+                                <input
+                                    type="password"
+                                    className="form-control rounded-pill"
+                                    id="password"
+                                    placeholder="Password "
+                                />
+                            </div>
+                            <button type="submit" className="btn btn-primary btn-lg btn-block rounded-pill">
+                                Sign Me Up
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
