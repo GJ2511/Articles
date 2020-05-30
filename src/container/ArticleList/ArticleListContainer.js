3d9eb425d8ea2b4d70e8473eae56dd11d98071e5
Gaurav Joshi <gaurav.joshi@onsolve.com>
2020-05-31 00:03:40 +0530

    improove design

--- src/container/ArticleList/ArticleListContainer.js
+++ src/container/ArticleList/ArticleListContainer.js
@@ -15,10 +15,10 @@ class ArticleListContainer extends Component {
     render() {
         return (
             <div className="row mt-5">
-                <div className="col-sm-12 col-md-3 col-lg-3 col-xl-4">
+                <div className="col-sm-12 col-md-6 col-lg-3 col-xl-3">
                     <FavTagContainer handleTagClick={this.onFavTagClick} />
                 </div>
-                <div className="col-sm-12 col-md-9 col-lg-9 col-xl-8">
+                <div className="col-sm-12 col-md-6 col-lg-9 col-xl-9">
                     <ArtileList />
                 </div>
             </div>
