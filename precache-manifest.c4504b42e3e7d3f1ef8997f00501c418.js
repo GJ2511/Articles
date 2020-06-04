ca7a1fb6de77673617266c6279ef07fdbc8749b0
Gaurav Joshi <gaurav.joshi@onsolve.com>
2020-06-04 16:22:51 +0530

    Updates

---
+++ precache-manifest.c4504b42e3e7d3f1ef8997f00501c418.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d1ac52c617adc7b964e46e75d6e5be3",
+    "url": "/Articles/index.html"
+  },
+  {
+    "revision": "c9c0bdfbe0aeebb57219",
+    "url": "/Articles/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "515a2070ccf6a5999515",
+    "url": "/Articles/static/css/main.faa31cba.chunk.css"
+  },
+  {
+    "revision": "c9c0bdfbe0aeebb57219",
+    "url": "/Articles/static/js/2.b247dcdf.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/Articles/static/js/2.b247dcdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "515a2070ccf6a5999515",
+    "url": "/Articles/static/js/main.573bf8a9.chunk.js"
+  },
+  {
+    "revision": "1a599db39cf1ceee0b48",
+    "url": "/Articles/static/js/runtime-main.42f05e10.js"
+  }
+]);
